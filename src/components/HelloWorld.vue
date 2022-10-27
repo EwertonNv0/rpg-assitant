f830efad77a4095aa4d03d66d3183405f2f00611
Ewerton Antonio <100003880+EwertonNv0@users.noreply.github.com>
2022-10-27 00:13:18 -0300

    incio

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+      msg2: 'Fãs do DJ Gil Bala',
+      contador: 0
+    }
+  },
+  methods:{
+    aumentaContador(){
+      this.contador+=1000
+    }
+  }
+}
+</script>
+
+<template>
+  <p>{{ msg2 }}</p>
+  <p>{{ contador }}</p>
+  <button @click='aumentaContador'>Aqui carai</button>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
