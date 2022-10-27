e076835cd53976b1afc0ba49f11ec19ca53bd8da
Ewerton Antonio <100003880+EwertonNv0@users.noreply.github.com>
2022-10-27 13:17:53 -0300

    Create AddServerStatic.vue

---
+++ src/components/AddServerStatic.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+    name: 'AddServerStatic'
+}
+</script>
+
+<template>
+    <a href="https://www.google.com/" target="_blank">
+        <button>
+            <img src="../assets/bad-cat.png" alt="bad cat" id="bad-cat">
+            Adicione ao seu Servidor.
+        </button>
+    </a>
+</template>
+
+<style scoped>
+a {
+    text-decoration: none;
+}
+button {
+    background: linear-gradient(-45deg, #be302c, #370e0c, #22060a, #eb220e);
+    background-size: 800% 400%;
+    padding: .8em 2em;
+    display: inline-flex;
+    border: none;
+    border-radius: 10px;
+    font-family: 'Roboto';
+    font-size: 1.1rem;
+    color: white;
+    transition: all .5s ease-in-out;
+    cursor: pointer;
+    align-items: center;
+}
+
+button:hover {
+    transform: scale(1.04);
+}
+
+button:active {
+    transform: scale(.95);
+}
+
+#bad-cat{
+    width: 1.5rem;
+    margin-right: .2rem;
+}
+</style>
